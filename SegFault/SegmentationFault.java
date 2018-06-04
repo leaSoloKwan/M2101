71322bba42cb569b6a403d6548b8683dadbf0260
VM IUT <vm@iut>
2018-06-04 15:26:33 +0200

    ficje7ex1

---
+++ SegFault/SegmentationFault.java
@@ -0,0 +1,7 @@
+public class SegmentationFault{
+	
+	public static void main(String[] args){
+		int[] tab=new int[5]; 
+		tab[10]=20;
+	}
+}
---
+++ SegFault/segmentation_fault.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+	int t[10];
+
+	t[10000]=1;
+	return 0;
+}
