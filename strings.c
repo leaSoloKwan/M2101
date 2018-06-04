dfcfd5bd8aba7d95a8fb218f0e209e14207fa966
VM IUT <vm@iut>
2018-06-04 14:21:24 +0200

    oups ptit prob exo

---
+++ strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void) {
+	char hello[] = "Bonjour";
+	char bye[11] = "Au revoir";
+	char numbers[10] = "012345678\0";
+
+	printf("hello: %s\n", hello);
+	printf("numbers: %s\n", numbers);
+	printf("numbers + 7: %s\n", numbers + 7);
+	printf("bye: %s\n", bye);
+
+	return 0;
+}
+
+
